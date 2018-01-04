5ca73339afebd29b7013dd765754d5133e2a8492
Seredenko Denis <psi.den@gmail.com>
2018-01-04 16:16:53 +0200

    fork project

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+require_once realpath(__DIR__ . '/vendor') . '/autoload.php';
+
+use numphp\np_array;
+use numphp\operator;
+
 $list = new np_array([18, 25, 26, 30, 34]);
 
 var_dump($list[operator::b_and($list->gt(25), $list->lt(30))]);
