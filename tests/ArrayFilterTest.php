5ddd2c13c25313c27034deb07468b729581b86e9
Seredenko Denis <psi.den@gmail.com>
2018-01-04 16:16:53 +0200

    fork project

---
+++ tests/ArrayFilterTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests;
+
+
+use numphp\np_array;
+use numphp\operator;
+
+class ArrayFilterTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var np_array
+     */
+    private $list;
+
+    public function setUp()
+    {
+        $this->list = new np_array([18, 25, 26, 30, 34]);
+    }
+
+    public function testFilter()
+    {
+        $res = $this->list[operator::b_and($this->list->gt(25), $this->list->lt(30))];
+        $this->assertEquals($res, [26]);
+    }
+}
